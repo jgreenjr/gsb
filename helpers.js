649efeefbdcbdaf48e27b497a6ea479ff5498f60
root <jgreenjr@gmail.com>
2015-02-16 16:42:18 -0800

    fixing summing for future items

--- helpers.js
+++ helpers.js
@@ -2,40 +2,17 @@ exports.UpdateTotal = function(currentTotal, transaction)
 {
     if(!currentTotal.ClearedBalance)
         currentTotal.ClearedBalance = 0
-        
+
     var includeInCleared = transaction.Status == "Cleared"|| transaction.Status == "Closed";
      switch(transaction.type){
             case "deposit":
-                if(!transaction.IsFutureItem)
-              {
-                  currentTotal.ActualBalance = currentTotal.ActualBalance + parseFloat(transaction.amount);
-              }
-              else
-                {
-                    
-                    if(!currentTotal.FutureBalance){
-                        currentTotal.FutureBalance = currentTotal.ActualBalance;
-                    }
-                    currentTotal.FutureBalance = currentTotal.FutureBalance + parseFloat(transaction.amount);
-                }
+              currentTotal.ActualBalance = currentTotal.ActualBalance + parseFloat(transaction.amount);
               if(includeInCleared)
                 currentTotal.ClearedBalance = currentTotal.ClearedBalance + parseFloat(transaction.amount);
-                
+
               break;
               case "widthdrawl":
-                if(!transaction.IsFutureItem)
-              {
               currentTotal.ActualBalance = currentTotal.ActualBalance - parseFloat(transaction.amount);
-              }
-               else
-                {
-                    
-                   
-                    if(!currentTotal.FutureBalance){
-                        currentTotal.FutureBalance = currentTotal.ActualBalance;
-                    }
-                    currentTotal.FutureBalance = currentTotal.FutureBalance - parseFloat(transaction.amount);
-                }
               if(includeInCleared)
                 currentTotal.ClearedBalance = currentTotal.ClearedBalance - parseFloat(transaction.amount);
               break;
